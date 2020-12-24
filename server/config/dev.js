ef1633402eb0d915db52b726280dbd63b16d392f
Arif Sardar <73414242+arif-2005@users.noreply.github.com>
2020-12-24 16:52:19 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI:'mongodb+srv://arif_sardar:$CosmuSury@202020@spi-chatcord.grusw.mongodb.net/SPI_CHATCORD?retryWrites=true&w=majority',
+  mongoURI:'mongodb+srv://arif_sardar:$CosmuSury@202020@spi-chatcord.grusw.mongodb.net/test?retryWrites=true&w=majority',
 }
